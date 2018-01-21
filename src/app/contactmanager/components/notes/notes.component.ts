9d9602426ebdb83b990083f2f024c090beb6fd41
Henry <henrygustavof@gmail.com>
2018-01-21 15:06:26 -0500

    Data table

---
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Note } from '../../models/note';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
+})
+export class NotesComponent implements OnInit {
+
+  @Input() notes: Note[];
+
+  displayedColumns = ['position', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+
+  constructor() { }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+}
